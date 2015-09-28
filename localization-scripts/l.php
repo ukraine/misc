c7ddcb4b341ddabf8dd3335e13e115eb1e8cc89d
Yuriy <ua@ifstudio.org>
2015-09-28 11:56:47 +0300

    quick stats shown

--- localization-scripts/l.php
+++ localization-scripts/l.php
@@ -2,21 +2,30 @@
 
 error_reporting(E_ALL);
 
+$lang['totalFiles']  = "Total text strings: ";
+
 // debug
 // foreach($_GET as $key=>$val) {echo "$key=>$val<br>";}
 
 include $_GET['f'];
 
+$totalStrings = "Total strings: ";
 $langVarName = "language";
+$type = "val";
 
 if (!empty($_GET['langVarName'])) $langVarName = $_GET['langVarName'];
+if (!empty($_GET['type'])) $type = $_GET['type'];
+
+echo $totalStrings . count(${$langVarName}) . "<br><br>";
 
 foreach (${$langVarName} as $key=>$val) {
 
-	switch($_GET['type']) {
+	switch($type) {
 	
 		default: 	echo "$val<br>"; break;
 		case "key": 	echo "$key<br>"; break;
+		case "formattedKey": 	echo "'$key' =><br>"; break;
+		case "formattedVal": 	echo "'$val',<br>"; break;
 
 	}
 
